ba606c1dc6b0543f151c6d581d63f2b4212e298b
fatihpur <fatihnuripur@gmail.com>
2025-07-29 15:23:46 +0300

    ok

---
+++ src/main/java/Data/Data.java
@@ -0,0 +1,15 @@
+package Data;
+
+import org.openqa.selenium.WebDriver;
+
+public class Data {
+    public static WebDriver driver;
+
+    public String registerName = "TestName";
+    public String registerSurname = "TestSurname";
+    public String registerEmail = "1otomasyontest1@gmail.com";
+    public String registerPhone = "5458524351";
+    public String registerPassword = "123456";
+    public String registerRepeatPassword = "123456";
+    public String registerUrl = "https://qrsofra.com/auth/jwt/sign-up";
+}
