9840854f23d7d90d6f212ff162c9660a3e60fe0a
fatihpur <fatihnuripur@gmail.com>
2025-08-04 18:26:12 +0300

    final

--- src/main/java/Data/Data.java
+++ src/main/java/Data/Data.java
@@ -2,8 +2,8 @@ package Data;
 public class Data {
     public String registerName = "TestName";
     public String registerSurname = "TestSurname";
-    public String registerEmail = "3otomasyontest3@gmail.com";
-    public String registerPhone = "5456524951";
+    public String registerEmail = "5otomasyontest5@gmail.com";
+    public String registerPhone = "5496524951";
     public String registerPassword = "123456";
     public String registerRepeatPassword = "123456";
     public String registerUrl = "https://qrsofra.com/auth/jwt/sign-up";
